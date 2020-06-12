6e562f49345597b081a986f28efb2a223988fc69
Dusten Peterson <>
2020-06-11 19:23:46 -0600

    - Adding some basic styling and social links.

--- personal-site/src/app/app.component.ts
+++ personal-site/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'personal-site';
+export class AppComponent implements OnInit {
+  pageLoaded = false;
+
+  ngOnInit() {
+    this.pageLoaded = true;
+  }
 }
