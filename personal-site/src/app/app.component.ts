c81b2b6a01690d2978daee6aeb7bf778678ff4f7
Dusten Peterson <>
2020-06-11 19:36:24 -0600

    - Updating styles.

--- personal-site/src/app/app.component.ts
+++ personal-site/src/app/app.component.ts
@@ -1,14 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  pageLoaded = false;
-
-  ngOnInit() {
-    this.pageLoaded = true;
-  }
-}
+export class AppComponent { }
